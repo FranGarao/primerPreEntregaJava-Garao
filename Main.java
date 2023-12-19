ed45fef59fb106726ea0a3ae3c66063ada0d72d2
FGdev <garaofran@gmail.com>
2023-12-19 14:34:17 -0300

    primerEntrega

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        float numero = 2.6f;
+        int numeroEntero = (int) numero;
+        System.out.println(numeroEntero);
+        float numeroDecimal = numero - (float) numeroEntero;
+        System.out.println(numeroDecimal);
+    }
+}
